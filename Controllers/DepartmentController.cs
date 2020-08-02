843194e2f5b2974494a1e31f9578c3f057b27f0b
David <dsilvestri@australtek.com>
2020-08-02 13:27:08 -0300

    Add project files.

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebAPI.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+
+            //string query = @"select DepartmentID, DepartmentName from dbo.Departments ";
+            string query = @"select * from dbo.Departments ";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query,con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        public string Post(Department dep)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"insert into dbo.Departments values ('" +dep.DepartmentName + @"')";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+
+
+                return "Added Successfully";
+            }
+            catch (Exception)
+            {
+                return "Fail to Add";
+            }
+        }
+
+        public string Put(Department dep)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"
+                    Update dbo.Departments set DepartmentName =  '" + dep.DepartmentName + @"'
+                    Where DepartmentID = " + dep.DepartmentID + @"
+                    ";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+
+
+                return "Updated Successfully";
+            }
+            catch (Exception)
+            {
+                return "Fail to Add";
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"
+                    delete from dbo.Departments where DepartmentID = " + id;
+
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+
+
+                return "Deleted Successfully";
+            }
+            catch (Exception)
+            {
+                return "Fail to Add";
+            }
+        }
+
+
+    }
+}
